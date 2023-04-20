9c462fab0ce63ad680ac9ecf2746533d0c9de3a0
Danny Osmar Sevilla Martinez <dsevilla@ni.thedataage.com>
2023-04-19 20:55:59 -0600

    upgrade codelabs

---
+++ bin/codelabs_5.dart
@@ -0,0 +1,5 @@
+import 'package:codelabs_5/codelabs_5.dart' as codelabs_5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${codelabs_5.calculate()}!');
+}
---
+++ test/codelabs_5_test.dart
@@ -0,0 +1,8 @@
+import 'package:codelabs_5/codelabs_5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
