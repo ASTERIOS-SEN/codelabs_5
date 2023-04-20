58d4370a748e3473eda6c34741b6b27903166640
Danny Osmar Sevilla Martinez <dsevilla@ni.thedataage.com>
2023-04-19 21:20:55 -0600

    upgrade codelabs

--- bin/codelabs_5.dart
+++ bin/codelabs_5.dart
@@ -1,5 +1,12 @@
 import 'package:codelabs_5/codelabs_5.dart' as codelabs_5;
+import 'futures.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${codelabs_5.calculate()}!');
+  //Ejercicio: Practique el uso de async y await
+  returnFuture();
+  print('');
+
+  //Ejercicio: poner todo junto
+  returnFuture2();
+  print('');
 }
---
+++ bin/futures.dart
@@ -0,0 +1,85 @@
+Future<void> fetchUserOrder() {
+  // Imagine that this function is fetching user info from another service or database.
+  return Future.delayed(const Duration(seconds: 2), () => print('Large Latte'));
+}
+
+void future() {
+  fetchUserOrder();
+  print('Fetching user order...');
+}
+
+
+//Ejercicio: Practique el uso de async y await
+
+Future<String> fetchRole() async {
+  // Simulamos una espera de 2 segundos para obtener el nombre de usuario
+  await Future.delayed(Duration(seconds: 2));
+  return 'Juan'; // Retorna el nombre de usuario
+}
+
+Future<int> fetchLoginAmount() async {
+  // Simulamos una espera de 3 segundos para obtener la cantidad de logins
+  await Future.delayed(Duration(seconds: 3));
+  return 10; // Retorna la cantidad de logins
+}
+
+Future<String?> reportUserRole() async {
+  try{
+   var username = await fetchRole();
+   return 'User role: $username';
+  } catch (err) {
+    print('Caught error: $err');
+  }
+ }
+
+Future<String?> reportLogins() async {
+  try{
+   var logins = await fetchLoginAmount();
+   return 'Total number of logins: $logins';
+  } catch (err) {
+    print('Caught error: $err');
+  }
+  
+}
+
+void returnFuture() async {
+  print(await reportUserRole());
+  print(await reportLogins());
+}
+
+
+
+Future<String> fetchUsername() async {
+  // Simulamos una espera de 2 segundos para obtener el nombre de usuario
+  await Future.delayed(Duration(seconds: 2));
+  return 'Juan'; // Retorna el nombre de usuario
+}
+
+Future<int> logoutUser() async {
+  // Simulamos una espera de 3 segundos para obtener la cantidad de logins
+  await Future.delayed(Duration(seconds: 3));
+  return 10; // Retorna la cantidad de logins
+}
+
+
+//Ejercicio: poner todo junto
+String addHello(String user) => 'Hello $user';
+
+Future<String> greetUser() async {
+  var username = await fetchUsername();
+  return addHello(username);
+}
+
+Future<String> sayGoodbye() async {
+  try {
+    var result = await logoutUser();
+    return '$result Thanks, see you next time';
+  } catch (e) {
+    return 'Failed to logout user: $e';
+  }
+}
+
+void returnFuture2() async {
+  print(await greetUser());
+  print(await sayGoodbye());
+}
